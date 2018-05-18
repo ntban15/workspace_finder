7893372801ce118fd8db35640039152360774465
ntban15 <ngtab.an.9@gmail.com>
2018-05-18 17:34:08 +0700

    Readd API

---
+++ src/constants/apis.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyD51wNw_IDL6kIzeir__2Bl448aaoLkahs',
+  authDomain: 'social-network-f437d.firebaseapp.com',
+  databaseURL: 'https://social-network-f437d.firebaseio.com',
+  projectId: 'social-network-f437d',
+  storageBucket: 'social-network-f437d.appspot.com',
+  messagingSenderId: '933040599154',
+};
