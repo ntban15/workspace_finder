b2f407e457ad0c4e9a90d048780059310cfdf3d6
ntban15 <ngtab.an.9@gmail.com>
2018-05-18 08:41:57 +0700

    Remove exposed information

--- src/constants/apis.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyD51wNw_IDL6kIzeir__2Bl448aaoLkahs',
-  authDomain: 'social-network-f437d.firebaseapp.com',
-  databaseURL: 'https://social-network-f437d.firebaseio.com',
-  projectId: 'social-network-f437d',
-  storageBucket: 'social-network-f437d.appspot.com',
-  messagingSenderId: '933040599154',
-};
